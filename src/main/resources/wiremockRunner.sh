2c078684223d5fc9c21439bd8f35d92a4c71a910
Juan Camilo Dvera <juan.dvera@appgate.com>
2020-12-15 19:35:28 -0500

    fix wiremockRunner.sh

--- src/main/resources/wiremockRunner.sh
+++ src/main/resources/wiremockRunner.sh
@@ -4,6 +4,6 @@ mkdir wiremock && cd wiremock
 wget https://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-jre8-standalone/2.27.2/wiremock-jre8-standalone-2.27.2.jar .
 mkdir mappings
 cp ../src/main/resources/mapping.json mappings/.
-chmod +x wiremock-standalone-2.27.2.jar
+chmod 777 ./wiremock-standalone-2.27.2.jar
 java -jar wiremock-standalone-2.27.2.jar --port 8081 &
 
